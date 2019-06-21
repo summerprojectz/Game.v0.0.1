fc0dc2d511af5006c53a438af5c02b486cdcce67
Calum Mathison <40406464@live.napier.ac.uk>
2019-06-21 21:43:46 +0100

    Added Basic Objects
    Added GameManager, CombatManager and a test player/enemy.

---
+++ GameV_0_0_1/Assets/Scripts/CombatManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    #region Fields
+    #endregion
+
+    #region Properties
+    #endregion
+
+    #region Methods
+    #endregion
+}
---
+++ GameV_0_0_1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Fields
+    public GameObject player;
+    public GameObject target;
+
+    bool _activateCombat;
+
+    #endregion
+
+    #region Properties
+    #endregion
+
+    #region Methods
+    private void Start()
+    {
+        _activateCombat = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForCombat();
+    }
+
+    private void CheckForCombat()
+    {
+        if (Vector3.Distance(player.transform.position, target.transform.position) < 10)
+        {
+            _activateCombat = true;
+        }
+    }
+    #endregion
+}
