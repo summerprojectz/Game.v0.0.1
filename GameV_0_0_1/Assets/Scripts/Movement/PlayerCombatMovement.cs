7cc6a017850acf05a9f3b48e8ce17e365fb80b89
Calum Mathison <40406464@live.napier.ac.uk>
2019-06-24 23:29:14 +0100

    Added Agent movment

---
+++ GameV_0_0_1/Assets/Scripts/Movement/PlayerCombatMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerCombatMovement : MonoBehaviour
+{
+    #region Fields
+    NavMeshAgent _agent;
+    Camera _battleCam;
+    
+    #endregion
+
+    #region Properties 
+    #endregion
+
+    #region Methods
+    private void Start()
+    {
+        _agent = GameObject.FindGameObjectWithTag("Player").GetComponent<NavMeshAgent>();
+        _battleCam = GameObject.FindGameObjectWithTag("BattleCamera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = _battleCam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                _agent.SetDestination(hit.point);
+            }
+        }
+    }
+    #endregion
+}
